f4361bb7f93ee538eeb5bd8e911f0fb486a6e5a5
Mehulkumar Parmar <m3hul.g4m3r@gmail.com>
2025-05-10 05:06:23 +0530

    implement boolean value parser
    Signed-off-by: Mehulkumar Parmar <m3hul.g4m3r@gmail.com>

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,6 +10,12 @@ def parseNull(jsonString: String): Option[JsonParserValue[Null]] =
     case s"null$remString" => Some(JsonParserValue(null, remString))
     case _                 => None
 
+def parseBool(jsonString: String): Option[JsonParserValue[Boolean]] =
+  jsonString match
+    case s"true$remString"  => Some(JsonParserValue(true, remString))
+    case s"false$remString" => Some(JsonParserValue(false, remString))
+    case _                  => None
+
 @main def main(): Unit =
   val jsonString = """{"primes": [2, 3, 5], "evens": [2, 4, 6]}"""
   val map: Map[String, List[Int]] =
@@ -19,5 +25,11 @@ def parseNull(jsonString: String): Option[JsonParserValue[Null]] =
   val jsonNullString = """null123abc"""
   val jsonNullData = parseNull(jsonNullString)
   jsonNullData match
+    case Some(value) => println(value)
+    case None        => println("no value")
+
+  val jsonBoolString = """false123abc"""
+  val jsonBoolData = parseBool(jsonBoolString)
+  jsonBoolData match
     case Some(value) => println(value)
     case None        => println("no value")
